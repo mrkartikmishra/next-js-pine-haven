add8006e87357f1a8924383d1e87162828ac7894
Kartik Mishra <kartikmishra.km44@gmail.com>
2025-07-25 15:26:09 +0530

    home page

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -5,7 +5,7 @@ import logo from "@/public/logo.png";
 export default function Logo() {
   return (
     <Link href={"/"} className="z-10 flex items-center gap-4">
-      <Image src={logo} alt="The Pine haven" width={60} height={60} />
+      <Image src={logo} alt="The Pine haven logo" width={60} height={60} />
       <span className="text-xl font-semibold [word-spacing:-4px] text-primary-100">
         The Pine Haven
       </span>
